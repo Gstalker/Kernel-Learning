b50dec8c6f15e025b199801451f481ca8aadfc42
Gstalker <dreamstalker1003@qq.com>
2020-06-25 11:00:18 -0700

    写了一个shell来根据当前章节（目前只有第四章有，前面的待填坑）自动化生成hd60M.img

--- Cpt4.Rudiment of Protect Mode/run_bochs.sh
+++ Cpt4.Rudiment of Protect Mode/run_bochs.sh
@@ -1,7 +1,9 @@
 nasm -o ./mbr.bin ./mbr.s
 nasm -o ./loader.bin ./loader.s
 cd ..
+./bochs/bin/bximage -mode=create -hd=60M -imgmode="flat" -q ./bochs/bin/hd60M.img
 dd if=./Cpt4.Rudiment\ of\ Protect\ Mode/mbr.bin of=./bochs/bin/hd60M.img bs=512 count=1 conv=notrunc
 dd if=./Cpt4.Rudiment\ of\ Protect\ Mode/loader.bin of=./bochs/bin/hd60M.img bs=512 count=4 conv=notrunc seek=2
 cd ./bochs/bin
 ./bochs -f ./bochsrc.disk
+rm ./hd60M.img
